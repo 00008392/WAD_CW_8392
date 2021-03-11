af514f7948fa24c14c3702fe74e28c998aefc6c2
lnmrtz <00008392wiut@gmail.com>
2021-03-11 03:08:11 -0800

    CRUD for all models before refactoring

---
+++ WAD.8392.WebApp/Controllers/ProductCategoriesController.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WAD._8392.DAL.Context;
+using WAD._8392.DAL.DBO;
+
+namespace WAD._8392.WebApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductCategoriesController : ControllerBase
+    {
+        private readonly MusicInstrumentsDbContext _context;
+
+        public ProductCategoriesController(MusicInstrumentsDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/ProductCategories
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ProductCategory>>> GetProductCategories()
+        {
+            return await _context.ProductCategories.ToListAsync();
+        }
+
+        // GET: api/ProductCategories/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ProductCategory>> GetProductCategory(int id)
+        {
+            var productCategory = await _context.ProductCategories.FindAsync(id);
+
+            if (productCategory == null)
+            {
+                return NotFound();
+            }
+
+            return productCategory;
+        }
+
+        // PUT: api/ProductCategories/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutProductCategory(int id, ProductCategory productCategory)
+        {
+            if (id != productCategory.ProductCategoryId)
+            {
+                return BadRequest();
+            }
+            if(!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            _context.Entry(productCategory).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ProductCategoryExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/ProductCategories
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<ProductCategory>> PostProductCategory(ProductCategory productCategory)
+        {
+            if(!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            _context.ProductCategories.Add(productCategory);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetProductCategory", new { id = productCategory.ProductCategoryId }, productCategory);
+        }
+
+        // DELETE: api/ProductCategories/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteProductCategory(int id)
+        {
+            var productCategory = await _context.ProductCategories.FindAsync(id);
+            if (productCategory == null)
+            {
+                return NotFound();
+            }
+
+            _context.ProductCategories.Remove(productCategory);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool ProductCategoryExists(int id)
+        {
+            return _context.ProductCategories.Any(e => e.ProductCategoryId == id);
+        }
+    }
+}
---
+++ WAD.8392.WebApp/Controllers/ProductSubcategoriesController.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WAD._8392.DAL.Context;
+using WAD._8392.DAL.DBO;
+
+namespace WAD._8392.WebApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductSubcategoriesController : ControllerBase
+    {
+        private readonly MusicInstrumentsDbContext _context;
+
+        public ProductSubcategoriesController(MusicInstrumentsDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/ProductSubcategories
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ProductSubcategory>>> GetProductSubcategories()
+        {
+            return await _context.ProductSubcategories.ToListAsync();
+        }
+
+        // GET: api/ProductSubcategories/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ProductSubcategory>> GetProductSubcategory(int id)
+        {
+            var productSubcategory = await _context.ProductSubcategories.FindAsync(id);
+
+            if (productSubcategory == null)
+            {
+                return NotFound();
+            }
+
+            return productSubcategory;
+        }
+
+        // PUT: api/ProductSubcategories/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutProductSubcategory(int id, ProductSubcategory productSubcategory)
+        {
+            if (id != productSubcategory.ProductSubcategoryId)
+            {
+                return BadRequest();
+            }
+            if(!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            _context.Entry(productSubcategory).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ProductSubcategoryExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/ProductSubcategories
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<ProductSubcategory>> PostProductSubcategory(ProductSubcategory productSubcategory)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            _context.ProductSubcategories.Add(productSubcategory);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetProductSubcategory", new { id = productSubcategory.ProductSubcategoryId }, productSubcategory);
+        }
+
+        // DELETE: api/ProductSubcategories/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteProductSubcategory(int id)
+        {
+            var productSubcategory = await _context.ProductSubcategories.FindAsync(id);
+            if (productSubcategory == null)
+            {
+                return NotFound();
+            }
+
+            _context.ProductSubcategories.Remove(productSubcategory);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool ProductSubcategoryExists(int id)
+        {
+            return _context.ProductSubcategories.Any(e => e.ProductSubcategoryId == id);
+        }
+    }
+}
